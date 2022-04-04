c6c07b588a5edbfbe77a98d72fe3c6560e74ace6
Максим Дианов <shot_play@mail.ru>
2022-04-04 09:26:59 +0300

    innit

---
+++ Service/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storage;
+
+namespace Service
+{
+    public class Class1
+    {
+        public static bool Login(string login, string password)
+        {
+            Model1Container DataBase = new Model1Container();
+            var user = DataBase.Users.FirstOrDefault(x => x.Login == login);
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (user.Password == password)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
